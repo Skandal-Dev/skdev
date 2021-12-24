f666a425c7a3a57acf878c4dfed593f49f14bd2e
Skdev <alexandre.mtimet@gmail.com>
2021-12-24 10:53:39 +0100

    [CMPNT][CmdArea][Style] Create cmd body area component, and update design

---
+++ src/pages/cmd-area.js
@@ -0,0 +1,59 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Competence from "./competence";
+
+class CmdArea extends React.Component {
+
+    render() {
+        return (
+            <div class="code-area">
+            <div class="text-label">
+                Home
+            </div>
+            <div class="text-area">
+                <code>help</code>
+                <br />
+                <code className="output">
+                Help - Show all commands <br />
+                Go [PAGE] - Navigate to a definate page <br />
+                Ls ([-a]) - Display all pages <br />
+                Music - Pop up Music player <br />
+                Gif - Pop up random Gif <br />
+                Contact [mail] [title] [message] - Send me an email <br />
+                Clear - Clear terminal <br />
+                </code>
+            </div>
+            <div class="text-area">
+            <code>ls</code>
+            <br />
+            <code className="output">
+            <ul>
+                <li><span className="fetch">Home</span></li>
+                <li><span className="fetch">CV</span></li>
+                <li><span className="fetch">Projets</span></li>
+                <li><span className="fetch">Passions</span></li>
+                <li><span className="fetch">Portfolio</span></li>
+                <li><span className="fetch">Contact</span></li>
+            </ul>
+            </code>
+            </div>
+            <div class="text-area">
+                <code>go CV</code>
+                <br />
+                <code className="output">Moving to CV ...</code>
+                <Competence />
+            </div>
+
+            <div class="text-area">
+                <code>go CV</code>
+                <br />
+                <code className="output">Moving to CV ...</code>
+            </div>
+        
+            
+            </div>
+        );       
+    }
+}
+
+export default CmdArea;
